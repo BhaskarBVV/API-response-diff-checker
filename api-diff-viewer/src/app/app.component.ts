0057079d9161474affbfa84f4aca98d8d9f8c737
BhaskarBVV <71118230+BhaskarBVV@users.noreply.github.com>
2025-03-29 23:51:53 +0530

    [Feat]: make compare button start working. (#2)
    * [Feat]: Create new component to get the API information from user
    
    * [feat]: make the compare button working

--- api-diff-viewer/src/app/app.component.ts
+++ api-diff-viewer/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ApiRequestComponent } from './api-request/api-request.component';
 import { CompareResponseComponent } from './compare-response/compare-response.component';
 
@@ -10,4 +10,17 @@ import { CompareResponseComponent } from './compare-response/compare-response.co
 })
 export class AppComponent {
   title = 'api-diff-viewer';
+  @ViewChild('leftApi') leftApi!: ApiRequestComponent;
+  @ViewChild('rightApi') rightApi!: ApiRequestComponent;
+  @ViewChild('compareResponse') compareResponse!: CompareResponseComponent
+
+  leftRequestData: any;
+  rightRequestData: any;
+
+  compareResponses() {
+    this.leftRequestData = this.leftApi.getRequestData();
+    this.rightRequestData = this.rightApi.getRequestData();
+    this.compareResponse.getApiinfo(this.leftRequestData, this.rightRequestData)
+  }
+
 }
